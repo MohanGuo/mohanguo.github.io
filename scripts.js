835458fb1fb6e827dc49f1432fc05530294ef841
MohanGuo <mohan.guo@student.uva.nl>
2024-02-06 12:13:35 +0100

    1

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     function hideAllSections() {
         sections.forEach(section => {
             section.classList.add('hidden');
-            section.classList.add('section-style');
         });
     }
 
@@ -16,11 +15,20 @@ document.addEventListener('DOMContentLoaded', function() {
             const currentId = this.getAttribute('href').slice(1);
             const currentSection = document.getElementById(currentId);
             currentSection.classList.remove('hidden');
-            currentSection.classList.remove('section-style');
         });
     });
 
     // Optionally show the first section by default
     sections[0].classList.remove('hidden');
-    sections[0].classList.remove('section-style');
 });
+// document.addEventListener('DOMContentLoaded', function() {
+//     // 获取导航链接和要显示的section
+//     var contactLink = document.getElementById('contact-link');
+//     var contactSection = document.getElementById('contact');
+
+//     contactLink.addEventListener('click', function(e) {
+//         e.preventDefault(); // 阻止链接默认的导航行为
+//         // 切换contact section的可见状态
+//         contactSection.classList.toggle('hidden');
+//     });
+// });
